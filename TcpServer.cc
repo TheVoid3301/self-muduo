1f10271ee5733892ba828b91587a8cfaac21783d
TheVoid3301 <1449812768@qq.com>
2025-10-08 00:05:59 +0800

    整合TcpServer

--- TcpServer.cc
+++ TcpServer.cc
@@ -1,1 +1,54 @@
 #include "TcpServer.h"
+#include "Logger.h"
+
+EventLoop* CheckLoopNotNull(EventLoop *loop)
+{
+    if (loop == nullptr)
+    {
+        LOG_FATAL("%s:%s:%d mainLoop is null! \n", __FILE__, __FUNCTION__, __LINE__);
+    }
+    return loop;
+}
+
+TcpServer::TcpServer(EventLoop *loop, const InetAddress &listenAddr, const std::string &nameArg, Option option)
+    : loop_(CheckLoopNotNull(loop))
+    , ipPort_(listenAddr.toIpPort())
+    , name_(nameArg)
+    , acceptor_(new Acceptor(loop, listenAddr, option == kReusePort))
+    , threadPool_(new EventLoopThreadPool(loop, name_))
+    , connectionCallback_(defaultConnectionCallback)
+    , messageCallback_(defaultMessageCallback)
+    , writeCompleteCallback_(defaultWriteCompleteCallback)
+{
+
+}
+
+TcpServer::~TcpServer()
+{
+
+}
+
+void TcpServer::setThreadNum(int numThreads)
+{
+
+}
+
+void TcpServer::start()
+{
+
+}
+
+void TcpServer::newConnection(int sockfd, const InetAddress &peerAddr)
+{
+
+}
+
+void TcpServer::removeConnection(const TcpConnectionPtr &conn)
+{
+
+}
+
+void TcpServer::removeConnectionInLoop(const TcpConnectionPtr &conn)
+{
+
+}
